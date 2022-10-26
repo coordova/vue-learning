cf27f51382ddeeb6eb6ed9073f27c4357272e007
coordova <grcordova@gmail.com>
2022-10-26 18:04:40 -0400

    Learn vue3 ep10 - It's All So Easy

--- js/components/AssignmentList.js
+++ js/components/AssignmentList.js
@@ -6,10 +6,20 @@ export default {
     },
     template: `
         <section v-show="assignments.length">
-            <h2 class="font-bold mb-2 uppercase font-medium">{{ title }}</h2>
+            <h2 class="font-bold mb-2 uppercase font-medium">{{ title }} <span class="ml-3">({{assignments.length}})</span></h2>
+            <div class="flex gap-2">
+                <button 
+                    @click="currentTag = tag" 
+                    v-for="tag in tags" 
+                    class="px-1 pb-px text-xs border border-lime-400 rounded"
+                    :class="{
+                        'border-lime-600 bg-lime-600 shadow-lime-200 shadow-sm': tag === currentTag
+                    }"
+                >{{ tag }}</button>
+            </div>
             
-            <ul class="border border-gray-600 divide-y divide-gray-600">
-                <assignment v-for = "assignment in assignments" :key="assignment.id" :assignment="assignment"></assignment>
+            <ul class="border border-gray-600 divide-y divide-gray-600 mt-6">
+                <assignment v-for = "assignment in filteredAssignments" :key="assignment.id" :assignment="assignment"></assignment>
                 <!-- <li 
                     v-for = "assignment in assignments"
                     :key="assignment.id"
@@ -26,5 +36,25 @@ export default {
     props: {
         assignments: Array,
         title: String
+    },
+
+    data() {
+        return {
+            currentTag: 'all'
+        }
+    },
+
+    computed: {
+        filteredAssignments() {
+            if (this.currentTag == 'all') {
+                return this.assignments;
+            }
+            return this.assignments.filter(a => a.tag == this.currentTag);
+        },
+
+        tags() {
+            // return un set de UNICOS valores que tiene la lista assignments en su campo 'tag'
+            return ['all', ...new Set(this.assignments.map(a => a.tag))]; // usamos destructuring '...' para obtener los values del conjunto
+        }
     }
 }

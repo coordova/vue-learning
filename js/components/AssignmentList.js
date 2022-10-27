957c3806294d13f9e7e95c6bcbf74139657a5bb1
coordova <grcordova@gmail.com>
2022-10-27 19:07:16 -0400

    Learn vue3 ep11 - Component Responsibility

--- js/components/AssignmentList.js
+++ js/components/AssignmentList.js
@@ -1,25 +1,37 @@
 import Assignment from "./Assignment.js";
+import AssignmentTags from "./AssignmentTags.js";
 
 export default {
     components: {
-        Assignment
+        Assignment,
+        AssignmentTags
     },
     template: `
         <section v-show="assignments.length">
             <h2 class="font-bold mb-2 uppercase font-medium">{{ title }} <span class="ml-3">({{assignments.length}})</span></h2>
-            <div class="flex gap-2">
+            
+            <assignment-tags 
+                :initial-tags="assignments.map(a => a.tag)" 
+                :current-tag="currentTag"
+                @change="currentTag = $event"
+            />
+            <!-- <div class="flex gap-2">
                 <button 
                     @click="currentTag = tag" 
                     v-for="tag in tags" 
                     class="px-1 pb-px text-xs border border-lime-400 rounded"
                     :class="{
-                        'border-lime-600 bg-lime-600 shadow-lime-200 shadow-sm': tag === currentTag
+                        'border-lime-600 bg-lime-600 shadow-lime-2c00 shadow-sm': tag === currentTag
                     }"
                 >{{ tag }}</button>
-            </div>
+            </div> -->
             
             <ul class="border border-gray-600 divide-y divide-gray-600 mt-6">
-                <assignment v-for = "assignment in filteredAssignments" :key="assignment.id" :assignment="assignment"></assignment>
+                <assignment 
+                    v-for = "assignment in filteredAssignments" 
+                    :key="assignment.id" 
+                    :assignment="assignment"
+                ></assignment>
                 <!-- <li 
                     v-for = "assignment in assignments"
                     :key="assignment.id"
@@ -45,6 +57,13 @@ export default {
     },
 
     computed: {
+        /* filteredAssignments() {
+            if (this.currentTag == 'all') {
+                return this.assignments;
+            }
+            return this.assignments.filter(a => a.tag == this.currentTag);
+        }, */
+
         filteredAssignments() {
             if (this.currentTag == 'all') {
                 return this.assignments;
@@ -52,9 +71,6 @@ export default {
             return this.assignments.filter(a => a.tag == this.currentTag);
         },
 
-        tags() {
-            // return un set de UNICOS valores que tiene la lista assignments en su campo 'tag'
-            return ['all', ...new Set(this.assignments.map(a => a.tag))]; // usamos destructuring '...' para obtener los values del conjunto
-        }
+
     }
 }
---
+++ js/components/AssignmentTags.js
@@ -0,0 +1,35 @@
+export default {
+    template: `
+        <div class="flex gap-2">
+            <!-- <button 
+                @click="currentTag = this.currentTag" 
+                v-for="tag in tags" 
+                class="px-1 pb-px text-xs border border-lime-400 rounded"
+                :class="{
+                    'border-lime-600 bg-lime-600 shadow-lime-2c00 shadow-sm': tag === currentTag
+                }"
+            >{{ tag }}</button> -->
+            <button 
+                @click="$emit('change', tag)" 
+                v-for="tag in tags" 
+                class="px-1 pb-px text-xs border border-lime-400 rounded"
+                :class="{
+                    'border-lime-600 bg-lime-600 shadow-lime-2c00 shadow-sm': tag === currentTag
+                }"
+            >{{ tag }}</button>
+        </div>
+    `,
+
+    props: {
+        initialTags: Array,
+        currentTag: String,
+    },
+
+    computed: {
+        tags() {
+            // return un set de UNICOS valores que tiene la lista assignments en su campo 'tag'
+            // return ['all', ...new Set(this.assignments.map(a => a.tag))]; // usamos destructuring '...' para obtener los values del conjunto
+            return ['all', ...new Set(this.initialTags)]; // usamos destructuring '...' para obtener los values del conjunto
+        }
+    }
+}
